268f79549be407567df9d21590b74ef47a89e252
Vitali Filippow <v.filippow@micromata.de>
2016-09-14 20:47:15 +0200

    styling like in angularjs should be done

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -8,25 +8,31 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.dishes = "";
   $scope.message = "";
+  $scope.messageStyle = "";
+  $scope.borderColor = "";
   $scope.checkIfTooMuch = function() {
 
     // check for empty dishes
     if ($scope.dishes == false) {
       $scope.message = "Please enter data first";
-      showErrorView();
+      $scope.messageStyle = "red";
+      $scope.borderColor = "red";
       return;
     }
 
     var dishesSize = countValidDishes($scope.dishes);
     if(dishesSize === 0) {
       $scope.message = "Please enter data first";
-      showErrorView();
+      $scope.messageStyle = "red";
+      $scope.borderColor = "red";
     } else if(dishesSize <= 3) {
       $scope.message = "Enjoy!";
-      showSuccessView();
+      $scope.messageStyle = "green";
+      $scope.borderColor = "green";
     } else if(dishesSize > 3) {
       $scope.message = "Too Much!";
-      showSuccessView();
+      $scope.messageStyle = "green";
+      $scope.borderColor = "green";
     }
   };
 
@@ -37,16 +43,6 @@ function LunchCheckController($scope) {
 
   function checkIfStringIsEmpty(dish) {
     return dish.trim().length !== 0;
-  }
-
-  function showErrorView() {
-    document.getElementsByClassName("message")[0].style.color="red";
-    document.getElementById("lunch-menu").style.border="1px solid red";
-  }
-
-  function showSuccessView() {
-    document.getElementsByClassName("message")[0].style.color="green";
-    document.getElementById("lunch-menu").style.border="1px solid green";
   }
 }
 
